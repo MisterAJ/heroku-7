ceb5e4fab78d1c19cebcbdbcc98e1a1f33612a23
Marty Kunsman <kunsmanmartin@gmail.com>
2017-12-24 04:09:26 +0000

    First Commit.

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+// var express = require('express');
+ 
+// var app = express();
+var path = require('path');
+
+var port = process.env.PORT || 1337;
+console.log("Welcome to a live site!");
+
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+
+// var port = 1337;
+
+var server = http.createServer(function(request, response) {
+  console.log('log create Server');
+  response.writeHead(200, {'Content-Type': 'text/plain' });
+  response.end('Helloooo World\n');
+}).listen(port, function() {
+  console.log("SServer running at http://our site" + port + "/");
+});
+
